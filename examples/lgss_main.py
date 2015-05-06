4388bc85336a432841cb6d9e4e89669e60e14321
can-cs <christian.a.naesseth@gmail.com>
2015-05-06 09:30:02 +0200

    Added ESS (not IS weights-based) to lgss example

--- examples/lgss_main.py
+++ examples/lgss_main.py
@@ -13,7 +13,7 @@ from lgss_model_nsmc import *
 T = 100
 a = 0.9
 varV = 1.0
-varE = 0.01
+varE = 0.001
 
 x = np.zeros(T)
 y = np.zeros(T)
@@ -40,7 +40,7 @@ for t in range(T):
     Pfilt[t] = Ppred[t] - K*Ppred[t]
 
 Np = 100
-M = 1000
+M = 10
 mBS = lgss_bs(a, varV, varE, y)
 mFA = lgss_fa(a, varV, varE, y)
 mNSMC = lgss_nsmc(a, varV, varE, y, M)
@@ -61,22 +61,53 @@ nPF.runForward(resScheme='systematic')
 tEnd = time.clock()
 print 'Time elapsed Nested PF: ',tEnd-tStart
 
-# Mean
+# ESS
+I = 50
+bsMean = np.zeros((I,T))
+bsVar = np.zeros((I,T))
+bsESS = np.zeros(T)
+faMean = np.zeros((I,T))
+faVar = np.zeros((I,T))
+faESS = np.zeros(T)
+nMean = np.zeros((I,T))
+nVar = np.zeros((I,T))
+nESS = np.zeros(T)
+
+for i in range(I):
+    bsPF.runForward(resScheme='systematic')
+    bsMean[i,:] = bsPF.EX[:,0]-xfilt
+    
+    faPF.runForward(resScheme='systematic')
+    faMean[i,:] = faPF.EX[:,0]-xfilt
+    
+    nPF.runForward(resScheme='systematic')
+    nMean[i,:] = nPF.EX[:,0]-xfilt
+
+bsESS = Pfilt / np.var(bsMean,axis=0)
+faESS = Pfilt / np.var(faMean,axis=0)
+nESS = Pfilt / np.var(nMean,axis=0)
+
 figure()
-plot(xfilt)
-plot(bsPF.EX)
-plot(faPF.EX)
-plot(nPF.EX)
-print '(BS) Mean MSE: ',np.mean((bsPF.EX-xfilt)**2)
-print '(FA) Mean MSE: ',np.mean((faPF.EX-xfilt)**2)
-print '(NSMC) Mean MSE: ',np.mean((nPF.EX-xfilt)**2)
+semilogy(bsESS)
+semilogy(faESS)
+semilogy(nESS)
+    
+# Mean
+#figure()
+#plot(xfilt)
+#plot(bsPF.EX)
+#plot(faPF.EX)
+#plot(nPF.EX)
+#print '(BS) Mean MSE: ',np.mean((bsPF.EX-xfilt)**2)
+#print '(FA) Mean MSE: ',np.mean((faPF.EX-xfilt)**2)
+#print '(NSMC) Mean MSE: ',np.mean((nPF.EX-xfilt)**2)
 
 # Cov
-figure()
-plot(Pfilt)
-plot(bsPF.EX2 - bsPF.EX**2)
-plot(faPF.EX2 - faPF.EX**2)
-plot(nPF.EX2 - nPF.EX**2)
+#figure()
+#plot(Pfilt)
+#plot(bsPF.EX2 - bsPF.EX**2)
+#plot(faPF.EX2 - faPF.EX**2)
+#plot(nPF.EX2 - nPF.EX**2)
 print '(BS) Var MSE: ',np.mean((bsPF.EX2 - bsPF.EX**2-Pfilt)**2)
 print '(FA) Var MSE: ',np.mean((faPF.EX2 - faPF.EX**2-Pfilt)**2)
 print '(NSMC) Var MSE: ',np.mean((nPF.EX2 - nPF.EX**2-Pfilt)**2)
