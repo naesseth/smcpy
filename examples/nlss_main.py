f9de76608dfad472177457192b28e7fc3695a932
can-cs <christian.a.naesseth@gmail.com>
2015-05-06 16:04:00 +0200

    Added very basic Markov chain based NSMC

--- examples/nlss_main.py
+++ examples/nlss_main.py
@@ -6,12 +6,13 @@ import matplotlib
 import smc
 from nlss_model_bootstrap import *
 from nlss_model_nsmc import *
+from nlss_model_nsmc_mcmc import *
 
 # Generate data
 T = 100
 varx0 = 5. 
 varV = 10.
-varE = 1.
+varE = 1.0
 
 x = np.zeros(T)
 y = np.zeros(T)
@@ -24,20 +25,29 @@ for t in range(T):
     y[t] = x[t]**2/20. + np.sqrt(varE)*np.random.normal()
    
 Np = 100
-M = 100
 mBS = nlss_bs(varx0, varV, varE, y)
 bsPF = smc.smc(mBS,T,Np)
+
+M = 10
 mNSMC = nlss_nsmc(varx0, varV, varE, y, M)
 nPF = smc.smc(mNSMC,T,Np)
 
+kappa = 0.5
+K = 10
+mNSMCmcmc = nlss_nsmc_mcmc(varx0, varV, varE, y, M, K, kappa)
+nmcmcPF = smc.smc(mNSMCmcmc,T,Np)
 
-I = 25
+
+I = 10
 bsMean = np.zeros((I,T))
 bsVar = np.zeros((I,T))
 bsESS = np.zeros(T)
 nMean = np.zeros((I,T))
 nVar = np.zeros((I,T))
 nESS = np.zeros(T)
+nmcmcMean = np.zeros((I,T))
+nmcmcVar = np.zeros((I,T))
+nmcmcESS = np.zeros(T)
 
 for i in range(I):
     bsPF.runForward(resScheme='systematic')
@@ -48,9 +58,14 @@ for i in range(I):
     nMean[i,:] = nPF.EX[:,0]
     nVar[i,:] = (nPF.EX2 - nPF.EX**2)[:,0]
     
+    nmcmcPF.runForward(resScheme='systematic')
+    nmcmcMean[i,:] = nmcmcPF.EX[:,0]
+    nmcmcVar[i,:] = (nmcmcPF.EX2 - nmcmcPF.EX**2)[:,0]
+    
 bsESS = np.mean(bsVar,axis=0) / np.var(bsMean,axis=0)
 nESS = np.mean(nVar,axis=0) / np.var(nMean,axis=0)
-    
+nmcmcESS = np.mean(nmcmcVar,axis=0) / np.var(nmcmcMean,axis=0)
+   
 # Observations
 #figure()
 #plot(y)
@@ -60,13 +75,16 @@ figure()
 plot(x)
 plot(bsPF.EX)
 plot(nPF.EX)
+plot(nmcmcPF.EX)
 
 # Cov
-figure()
-plot(bsPF.EX2 - bsPF.EX**2,'g')
-plot(nPF.EX2 - nPF.EX**2,'r')
+#figure()
+#plot(bsPF.EX2 - bsPF.EX**2)
+#plot(nPF.EX2 - nPF.EX**2)
+#plot(nmcmcPF.EX2 - nmcmcPF.EX**2)
 
 # ESS
 figure()
-plot(bsESS,'g')
-plot(nESS,'r')
+plot(bsESS)
+plot(nESS)
+plot(nmcmcESS)
---
+++ examples/nlss_model_nsmc_mcmc.pyx
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+import sys
+sys.path.append("../src")
+import numpy as np
+cimport numpy as np
+cimport cython
+import resampling as res
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+
+class nlss_nsmc_mcmc():
+    r""" Example of the bootstrap formalism for sequential inference in a 
+    nonlinear time series model.
+    """
+    
+    def __init__(self, double varx0, double varV, double varE, np.ndarray[np.float64_t, ndim=1] y, int M, int K, double kappa):
+        self.dim = 1
+        self.varx0 = varx0
+        self.varE = varE
+        self.varV = varV
+        self.y = y
+        self.M = M
+        self.K = K
+        self.kappa = kappa
+        
+    def evalLogG(self, int t, np.ndarray[np.float64_t, ndim=2] xCur, np.ndarray[np.float64_t, ndim=2] xPrev, np.ndarray[np.float64_t, ndim=1] logV):
+        return np.zeros(xPrev.shape[0])
+        
+    def logY(self, np.ndarray[np.float64_t, ndim=1] X, int t):
+        return -0.5*(X**2/20. - self.y[t])**2/self.varE
+        
+    def logX(self, np.ndarray[np.float64_t, ndim=1] X, double xCond, int t):
+        return -0.5*(0.5*xCond + 25.*xCond/(1+xCond**2) + 8.*np.cos(1.2*float(t)) - X)**2/self.varV
+        
+    def mhrw(self, np.ndarray[np.float64_t, ndim=2] X, double xCond, double beta, int t):
+        cdef double cov = np.cov(X[:,0])
+        cdef np.ndarray[np.float64_t, ndim=2] Xprop = X
+        cdef np.ndarray[np.float64_t, ndim=2] Xout = X
+        
+        Xprop += np.sqrt(self.kappa*cov)*np.random.normal(size=(self.M,1))
+        
+        # Accept-Reject
+        cdef logU = np.log( np.random.rand(self.M) )
+        cdef logProp = beta*self.logY(Xprop[:,0],t) + self.logX(Xprop[:,0], xCond, t)
+        cdef logOld = beta*self.logY(X[:,0],t) + self.logX(X[:,0], xCond, t)
+        accept_index = logU <= logProp - logOld
+        
+        Xout[accept_index,:] = Xprop[accept_index,:]
+        return Xout
+    
+    def getZ(self, int t, np.ndarray[np.float64_t, ndim=2] xPrev):
+        cdef int N = xPrev.shape[0]
+        cdef int M = self.M
+        cdef int K = self.K
+        cdef double maxLogW = 0.
+        cdef double ESS = 1.
+        cdef np.ndarray[np.float64_t, ndim=3] X = np.zeros((N,M,self.dim))
+        cdef np.ndarray[np.float64_t, ndim=1] logW = np.zeros(M)
+        cdef np.ndarray[np.float64_t, ndim=2] w = np.zeros((N,M))
+        cdef np.ndarray[np.float64_t, ndim=1] logZ = np.zeros(N)
+        cdef np.ndarray[np.float64_t, ndim=1] beta = np.linspace(0,1,K+1)
+        cdef np.ndarray[np.int_t, ndim=1] ancestors = np.arange(M, dtype=int)
+        
+        for n in range(N):
+            # Sample from f(x_t|x_{t-1})
+            X[n,:,:] = 0.5*xPrev[n,0] + 25.*xPrev[n,0]/(1+xPrev[n,0]**2) + 8.*np.cos(1.2*t) + np.sqrt(self.varV)*np.random.normal(size=(M,self.dim))
+            
+            for k in range(1,K):
+                logW +=  (beta[k]-beta[k-1])*self.logY(X[n,:,0],t)
+                maxLogW = np.max(logW)
+                w[n,:] = np.exp(logW-maxLogW)
+                w[n,:] /= np.sum(w[n,:])
+                ESS = 1/np.sum(w[n,:]**2)
+                if ESS < 0.5*M:
+                    w[n,:] = np.exp(logW-maxLogW)
+                    logZ[n] += maxLogW + np.log(np.sum(w[n,:])) - np.log(M)
+                    w[n,:] /= np.sum(w[n,:])
+                    ancestors = res.resampling(w[n,:],'stratified')
+                    X[n,:,:] = self.mhrw(X[n,ancestors,:], xPrev[n,0], beta[k], t)
+                    logW = np.zeros(M)
+                    w[n,:] = 1./M
+            maxLogW = np.max(logW)
+            w[n,:] = np.exp(logW-maxLogW)
+            logZ[n] += maxLogW + np.log(np.sum(w[n,:])) - np.log(M)
+            w[n,:] /= np.sum(w[n,:])
+            
+        self.X = X
+        self.w = w
+        
+        return logZ
+
+    def simulate(self, np.ndarray[np.int_t, ndim=1] ancestors):
+        cdef int N = ancestors.shape[0] 
+        cdef np.ndarray[np.float64_t, ndim=2] Xout = np.zeros((N,self.dim))
+        cdef np.ndarray[np.float64_t, ndim=1] bins = np.zeros(self.M)
+        
+        for n in range(N):
+            bins = np.cumsum(self.w[ancestors[n],:])
+            Xout[n,:] = self.X[ancestors[n],np.digitize(np.random.random_sample(1), bins),:]
+        
+        return Xout
+  
+    def simM(self, int t, np.ndarray[np.float64_t, ndim=2] xPrev, np.ndarray[np.int_t, ndim=1] ancestors):
+        return self.simulate(ancestors)
+  
+    def evalAuxLogV(self, int t, np.ndarray[np.float64_t, ndim=2] xPrev):
+        return self.getZ(t, xPrev)
